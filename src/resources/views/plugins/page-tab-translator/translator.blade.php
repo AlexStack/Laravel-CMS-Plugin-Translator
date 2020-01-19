d31952adc08c72aec2d75a694a62efe7df762b4c
Alex <alex@10168.com>
2020-01-19 16:33:45 +1300

    New form layout

--- src/resources/views/plugins/page-tab-translator/translator.blade.php
+++ src/resources/views/plugins/page-tab-translator/translator.blade.php
@@ -1,27 +1,26 @@
 <div class="row mb-5 pb-5">
-    <div class="col-md-6">
+    <div class="col-md">
         @include($helper->bladePath('includes.form-input','b'), ['name' =>
         "translate_from", 'type'=>'select', 'options'=>$plugins['translator']['translate_languages'] ])
     </div>
-    <div class="col-md-6">
+    <div class="col-md">
         @include($helper->bladePath('includes.form-input','b'), ['name' =>
         "translate_to", 'type'=>'select', 'options'=>$plugins['translator']['translate_languages']])
     </div>
-    <div class="col-md-12">
-        @include($helper->bladePath('includes.form-input','b'), ['name' =>
-        "translate_content", 'type'=>'textarea' ])
-    </div>
-
-    <div class="col-md-6">
+    <div class="col-md">
         @include($helper->bladePath('includes.form-input','b'), ['name' =>
         "translate_result_add_to_field", 'type'=>'select', 'options'=>['main_content' => 'Main Content','sub_content' =>
         'Sub Content', 'extra_content_1'=> 'Extra Content 1', 'extra_content_2'=> 'Extra Content 2', 'extra_content_3'=>
         'Extra Content 3'] ])
     </div>
-    <div class="col-md-6 mb-5">
+    <div class="col-md">
         @include($helper->bladePath('includes.form-input','b'), ['name' =>
         "append_source_content", 'type'=>'select', 'options'=>['yes' => 'Yes',
         'no'=> 'No'] ])
     </div>
-
+    <div class="col-md-12">
+        @include($helper->bladePath('includes.form-input','b'), ['name' =>
+        "translate_content", 'type'=>'textarea' ])
+    </div>
 </div>
+<div class="pb-5 mb-5"></div>
